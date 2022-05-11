99d3d158f20cff432d03822364596c1504ca21f4
Ronald George <ronaldsamgeorge@gmail.com>
2022-05-12 02:08:00 +0530

    Add Display for Operations

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 
+
+// Changes the text displayed in the Calculator when button is pressed
 function display(data=0){
     const displayResult = document.querySelector('.display-out');
-    displayResult.textContent = data;
+    console.log(data);
+
+    if(displayResult.textContent === "0" || typeof data !== 'number'){
+        displayResult.textContent = data;
+    }else {
+        displayResult.textContent += data;
+    }
 }
 
+
+// When AC button is pressed and set the display are to 0
+function clearDisplay(){
+    const displayResult = document.querySelector('.display-out');
+    displayResult.textContent = 0;
+}
+
+// gets the  number or the operator clicked
 function getTextContent(nodeObject){
     const node = document.querySelector("#" + nodeObject.id);
-    const getNumber = node.childNodes;
-    const displayNumber = getNumber[1].textContent;
-    return Number(displayNumber);
+    const text = node.childNodes[1].textContent;
+    return (text);
 }
 
-function getNumber(button){
+function onNumber(button){
 
-    const textDisplay = getTextContent(this);
-    display(textDisplay);
-    
-   // display(this.id[lengthOfinput-1]);
+    const numberDisplay = getTextContent(this);
+    display(Number(numberDisplay));
 }
 
+
+function onOperations(){
+    const operationDisplay = getTextContent(this);
+    display(operationDisplay);
+}
+
+
+let operand = [];
+let operation = "";
 const numButtons = document.querySelectorAll('.numbers');
-numButtons.forEach(button => button.addEventListener('click', getNumber));
+numButtons.forEach(button => button.addEventListener('click', onNumber));
+
+const operationButtons = document.querySelectorAll('.operations');
+operationButtons.forEach(button => button.addEventListener('click', onOperations));
+
+const ac = document.querySelector('#ac');
+ac.addEventListener('click',clearDisplay);
 
 display();
